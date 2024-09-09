ff13f7be6e62a14b54154a7f84f1d580f24c50c0
Felipe <127968783+feduarte-dev@users.noreply.github.com>
2024-09-09 19:47:33 +0000

    question 1: done

---
+++ question1.js
@@ -0,0 +1,14 @@
+const sum = () => {
+    let INDICE = 12
+    let SOMA = 0
+    let K = 1
+
+    while (K < INDICE) {
+        K += 1
+        SOMA += K
+    }
+    return SOMA
+}
+
+
+console.log(sum())
